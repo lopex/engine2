bbf39504a2aeecaff6c4bef8f1437d492c3a3616
Marcin Mielzynski <lopx@gazeta.pl>
2016-04-27 20:17:29 +0200

    more meaningful names

--- lib/engine2/scheme.rb
+++ lib/engine2/scheme.rb
@@ -35,13 +35,13 @@ module Engine2
 
         define_scheme :create do |name = :create|
             define_action name, CreateMeta do
-                define_action :approve, InsertMeta
+                define_action :save, InsertMeta
             end
         end
 
         define_scheme :modify do |name = :modify|
             define_action name, ModifyMeta do
-                define_action :approve, UpdateMeta
+                define_action :save, UpdateMeta
             end
         end
 
