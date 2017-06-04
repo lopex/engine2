ac026a5ec2ace0eef46cae0f50770a87acca893f
Marcin Mielzynski <lopx@gazeta.pl>
2017-06-04 22:42:08 +0200

    it's done via access now

--- lib/engine2/meta/infra_meta.rb
+++ lib/engine2/meta/infra_meta.rb
@@ -256,7 +256,7 @@ module Engine2
             @meta[:fields] = [:name, :password]
             parent_meta = action.parent.*
             if parent_meta.is_a? MetaMenuSupport
-                parent_meta.menu(:menu).option :login_form, icon: :"log-in", disabled: 'action.action_pending()' #, show: "!action.logged_on"
+                parent_meta.menu(:menu).option :login_form, icon: :"log-in", disabled: 'action.action_pending()'
             end
         end
 
