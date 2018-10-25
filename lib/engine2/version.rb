6c00917d1cece760a803c97be08a9b2cd5abe7d0
Marcin Mielzynski <lopx@gazeta.pl>
2018-10-25 13:49:35 +0200

    bump version

--- lib/engine2/version.rb
+++ lib/engine2/version.rb
@@ -2,7 +2,7 @@
 # frozen_string_literal: true
 
 module Engine2
-    MAJOR, MINOR, TINY = [1, 0, 6]
+    MAJOR, MINOR, TINY = [1, 0, 7]
     VERSION = [MAJOR, MINOR, TINY].join('.').freeze
     def self.version
         VERSION
